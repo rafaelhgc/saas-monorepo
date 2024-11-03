0a9f4c99d19f40cf0475d9b061e298102c5a0360
rafaelhgc <gomes.rafaelcosta@gmail.com>
2024-11-03 12:15:52 -0300

    feat(agendaonline-landing): added landing page

--- apps/agendaonline-landing/src/app/modules/main-navigation/main-navigation.component.ts
+++ apps/agendaonline-landing/src/app/modules/main-navigation/main-navigation.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DSButtonDirective } from '@monorepo/design-system';
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet, DSButtonDirective],
+  imports: [RouterOutlet, DSButtonDirective, CommonModule],
   templateUrl: './main-navigation.component.html',
   styleUrls: ['./main-navigation.component.scss'],
 })
-export class MainNavigationComponent {}
+export class MainNavigationComponent {
+  get date() {
+    return new Date();
+  }
+}
