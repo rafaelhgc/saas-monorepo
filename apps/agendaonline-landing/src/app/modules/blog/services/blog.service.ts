ed442483f365a4c7a2920c0b0e8faf5d22b8a37d
Fabricio Serralvo <fabricio.serralvo@gmail.com>
2024-11-14 00:55:59 -0300

    more content

--- apps/agendaonline-landing/src/app/modules/blog/services/blog.service.ts
+++ apps/agendaonline-landing/src/app/modules/blog/services/blog.service.ts
@@ -8,6 +8,49 @@ export class BlogService {
   http = inject(HttpClient);
   posts = [
     {
+      title: 'Agendamento Online para Consultórios: Conforto e Eficiência',
+      coverImage: 'agendamento-online-para-consultorios-conforto-e-eficiencia.jpg',
+      slug: 'agendamento-online-para-consultorios-conforto-e-eficiencia',
+      createdAt: '2024-11-02T12:16:50.676Z',
+      tags: ['AgendamentoOnline', 'Consultórios', 'AgendaOnlineMe', 'Eficiência', 'GestãoDeHorários', 'Produtividade'],
+      related: [
+        {
+          title: 'Vantagens Competitivas do Agendamento Online',
+          coverImage: 'vantagens-competitivas-do-agendamento-online.jpg',
+          slug: 'vantagens-competitivas-do-agendamento-online',
+        },
+        {
+          title: 'Importância das Plataformas de Agendamento para Negócios',
+          coverImage: 'importancia-das-plataformas-de-agendamento.jpg',
+          slug: 'importancia-das-plataformas-de-agendamento',
+        },
+        { 
+          title: 'Agendamento Online e a Satisfação do Cliente',
+          coverImage: 'agendamento-online-e-a-satisfacao-do-cliente.jpg',
+          slug: 'agendamento-online-e-a-satisfacao-do-cliente',
+        }
+      ],
+    },
+    {
+      title: 'Benefícios do Agendamento Online para Salões de Beleza',
+      coverImage: 'beneficios-do-agendamento-online-para-saloes-de-beleza.jpg',
+      slug: 'beneficios-do-agendamento-online-para-saloes-de-beleza',
+      createdAt: '2024-10-30T00:16:50.676Z',
+      tags: ['AgendamentoOnline', 'SalaoDeBeleza', 'AgendaOnlineMe', 'Produtividade', 'GestãoDeTempo', 'AtendimentoAoCliente'],
+      related: [
+        {
+          title: 'Importância das Plataformas de Agendamento para Negócios',
+          coverImage: 'importancia-das-plataformas-de-agendamento.jpg',
+          slug: 'importancia-das-plataformas-de-agendamento',
+        },
+        { 
+          title: 'Agendamento Online e a Satisfação do Cliente',
+          coverImage: 'agendamento-online-e-a-satisfacao-do-cliente.jpg',
+          slug: 'agendamento-online-e-a-satisfacao-do-cliente',
+        }
+      ],
+    },
+    {
       title: 'Vantagens Competitivas do Agendamento Online',
       coverImage: 'vantagens-competitivas-do-agendamento-online.jpg',
       slug: 'vantagens-competitivas-do-agendamento-online',
